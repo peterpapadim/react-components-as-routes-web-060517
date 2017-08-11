f48f6cae21843c920ec7120e69ffdeb1cdbe3e60
Peter <ppapadim12@gmail.com>
2017-08-11 16:25:51 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,87 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'lightblue',
+  textDecoration: 'none',
+  color: 'white',
+}
+
+const Navbar = () =>
+  <div>
+    <NavLink
+      to="/"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'pink'
+      }}
+    >Home</NavLink>
+    <NavLink
+      to="/about"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'pink'
+      }}
+    >About</NavLink>
+    <NavLink
+      to="/login"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'pink'
+      }}
+    >Login</NavLink>
+    <NavLink
+      to="/messages"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'pink'
+      }}
+    >Messages</NavLink>
+  </div>;
+
+const Home = () => <h1>Home</h1>;
+
+const About = () => <h1>About</h1>;
+
+const Login = () =>
+  <form>
+    <h1>Login</h1>
+    <div>
+      <input type="text" name="username" placeholder="Username" />
+      <label htmlFor="username">Username</label>
+    </div>
+    <div>
+      <input type="password" name="password" placeholder="Password" />
+      <label htmlFor="password">Password</label>
+    </div>
+    <input type="submit" value="Login" />
+  </form>;
+
+const Messages = () => <h1>Messages</h1>
 
 const App = () => {
   return (
-    <div>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/messages" component={Messages}/>
+      </div>
+    </Router>
   );
 };
 
 ReactDOM.render(
-  <App />, 
+  <App />,
   document.getElementById('root')
 );
